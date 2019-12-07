ef0a5e2571dabb00b44c24916e4b7aeb979aee82
msiller <msiller93@yahoo.com>
2019-12-07 12:44:51 -0500

    updated all code to make this functioning

---
+++ app/data/friends.js
@@ -0,0 +1,19 @@
+var friendList = [
+  {
+    name: 'Super Mario',
+    profilePic: 'https://giantbomb1.cbsistatic.com/uploads/scale_medium/15/153607/3075875-mario.png',
+    scores: [
+      "4",
+      "5",
+      "5",
+      "2",
+      "4",
+      "5",
+      "4",
+      
+    ]
+  }
+]
+
+module.exports = friendList;
+  
